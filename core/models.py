5388e7dd9aa5183a0d366b9a3e4d5116a72ed06f
YusufAbdul <abdul17yusuf@gmail.com>
2022-07-26 20:48:06 +0100

    update

--- core/models.py
+++ core/models.py
@@ -198,7 +198,7 @@ class Favourite(models.Model):
     items = models.ManyToManyField(Item)
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
