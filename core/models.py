f13a864c4ec7f0f4a4c4e9598068f4fbfe3d74d0
YusufAbdul <abdul17yusuf@gmail.com>
2022-07-27 06:37:59 +0100

    frontend intergration started

--- core/models.py
+++ core/models.py
@@ -1,6 +1,3 @@
-from pyexpat import model
-from turtle import title
-from unicodedata import category
 from django.db import models
 from django.db.models.signals import post_save
 from django.conf import settings
